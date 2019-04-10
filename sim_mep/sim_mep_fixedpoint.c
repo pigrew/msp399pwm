ffb85312b4b95febcaccf1a6889e1926dbfe69ab
Nathan Conrad <pigrew@gmail.com>
2019-04-10 09:04:50 -0400

    Test code for checking MEP calculations.

---
+++ sim_mep/sim_mep_fixedpoint.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+
+int MEP_ScaleFactor;
+
+static uint32_t g_ratio = 0xB5FCDD7F;
+static uint32_t g_period = 16;
+
+static void pwm_applyRatio(uint16_t period);
+
+int main() {
+	MEP_ScaleFactor = 151;
+	for(int i=0; i<10; i++) {
+		g_ratio = 
+			 ((uint32_t)rand() & 0xFFFF)<<16
+			| (rand() & 0xFFFF) ;
+		pwm_applyRatio(g_period);
+	}
+	return 0;
+}
+
+#define MAX_32_D ((double)4294967295.0)
+
+static void pwm_applyRatio(uint16_t period) {
+	uint64_t high = ((uint64_t)g_ratio)*period*2*MEP_ScaleFactor; // Fractional part is last 32bits?
+	high += (1u<<31)-1u;
+	uint32_t count = high>>32;
+	//printf("g_ratio =         %8x\n", g_ratio);
+	//printf("high    = %16lx\n", high);
+	//printf("count   =         %8x\n", count);
+	uint32_t CMPA = count / (2*MEP_ScaleFactor);
+	uint32_t CMPB = CMPA;
+	
+	//printf("CMPA=%d, CMPb=%d\n", CMPA, CMPB);
+	count -= CMPA*(2*MEP_ScaleFactor);
+	uint32_t mepA = count/2;
+	uint32_t mepB = count-mepA;
+	if(mepB == MEP_ScaleFactor) {
+		CMPB = CMPA + 1;
+		mepB = 0;
+	}
+	
+	double r = ((double)g_ratio)/MAX_32_D;
+	double final = ((CMPA+mepA/(double)MEP_ScaleFactor)/(double)period +
+	                (CMPB+mepB/(double)MEP_ScaleFactor)/(double)period)/2.0;
+	//printf(" r=%f\n", final);
+	//printf("r0=%f\n", r);
+	double rminus = ((CMPA+mepA/(double)MEP_ScaleFactor)/(double)period +
+	                (CMPB+(mepB-1)/(double)MEP_ScaleFactor)/(double)period)/2.0;
+	double rplus = ((CMPA+mepA/(double)MEP_ScaleFactor)/(double)period +
+	                (CMPB+(mepB+1)/(double)MEP_ScaleFactor)/(double)period)/2.0;
+	double roffset = fabs(final-r);
+	double rplusoffset = fabs(rplus-r);
+	double rminusoffset = fabs(rminus-r);
+	
+	if((rplusoffset < roffset) || (rminusoffset < roffset) || mepA >= MEP_ScaleFactor) {
+		printf("FAIL %x\n", g_ratio);
+		printf("  r=%f\n", r);
+		printf(" rc=%f\n", final);
+		printf("\n");
+		printf(" ro=%f\n", roffset);
+		printf("r-o=%f\n", rminusoffset);
+		printf("r+o=%f\n", rplusoffset);
+		printf("\n");
+		printf("A=%d,B=%d,MEPA=%d,MEPB=%d\n",CMPA, CMPB, mepA, mepB);
+		printf("count0=%x\n", (uint32_t)(high>>32));
+		printf("count=%x\n", (mepA + mepB) + MEP_ScaleFactor*(CMPA+CMPB));
+		printf("------------------\n");
+		printf("\n");
+	}
+}
